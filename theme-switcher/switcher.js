cb0c6f6eeb7455bef5ff87531007330363e575a5
osabl <andrii.osabliuk@gmail.com>
2020-06-22 18:15:02 +0300

    add changing system theme

--- theme-switcher/switcher.js
+++ theme-switcher/switcher.js
@@ -2,12 +2,18 @@ try {
 	var shell = WScript.CreateObject('WScript.Shell')
   
   // '0' - dark theme, '1' - light theme
-	var currentTheme = shell.RegRead('HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\AppsUseLightTheme')
-	
+	var currentAppTheme = shell.RegRead('HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\AppsUseLightTheme')
+
 	if (currentTheme) {
+		// Apps theme
 		shell.RegWrite('HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\AppsUseLightTheme', '0', 'REG_DWORD')
+		// System theme
+		shell.RegWrite('HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\SystemUsesLightTheme', '0', 'REG_DWORD')
 	} else {
+		// Apps theme
 		shell.RegWrite('HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\AppsUseLightTheme', '1', 'REG_DWORD')
+		// System theme
+		shell.RegWrite('HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize\\SystemUsesLightTheme', '1', 'REG_DWORD')
 	}
 } catch (err) {
 	var shell = WScript.CreateObject('WScript.Shell')
